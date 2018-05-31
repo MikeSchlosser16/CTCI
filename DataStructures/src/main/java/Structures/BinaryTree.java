4228bdc425ac122e264ad27bdb8e4e5117dfeafb
Mike Schlosser <schlo201@d.umn.edu>
2018-05-30 23:03:03 -0500

    Add delete

--- DataStructures/src/main/java/Structures/BinaryTree.java
+++ DataStructures/src/main/java/Structures/BinaryTree.java
@@ -4,7 +4,7 @@ package Structures;
  * Created by MikeS on 5/29/18.
  */
 
-/* A binary tree must have the following properities:
+/* A binary tree must have the following properties:
     1. There is exactly one node in the tree which has no parent (root of the tree)
     2. Every other node in the tree has exactly one parent
     3. There can be no loops in a binary tree (it is not possible to follow a chain of pointers starting at the same node and arriving back at the same node)
@@ -16,10 +16,9 @@ package Structures;
     4. Leaf (no children, recoignized by left and right pointers being null)
     5. Subtree (we can consider any non-empty binary tree to be made up of a root node, a left subtree and a right subtree)
         - This is a recursive definition, so no suprise that recusrive subroutines often used to process trees
-
-
-
 */
+
+//BST
 public class BinaryTree {
 
     TreeNode root;
@@ -66,7 +65,7 @@ public class BinaryTree {
 
     private void insert(TreeNode node, Integer item){
         if(root == null){
-            node = new TreeNode(item, null, null);
+            root = new TreeNode(item, null, null);
         } else{
             if(item <= node.item){
                 insert(node.left, item);
@@ -74,6 +73,54 @@ public class BinaryTree {
                 insert(node.right, item);
             }
         }
+    }
+
+    public TreeNode delete(Integer item){
+        return delete(root, item);
+    }
+
+    private TreeNode delete(TreeNode node, Integer item){
+        if(node == null){
+            return null;
+        }
+
+        // Node to delete found
+        if(item == node.item){
+            /* 3 main different cases
+                1. Node has no children(just replace node with null in its parent node)
+                2. Node has exactly one child(in the parent node, we replace this node with its only child)
+                3. A node has two children(requires a tree reorganization)
+             */
+
+            // 1
+            if(node.left == null && node.right == null){
+                return null;
+            }
+            // 2
+            if(node.right == null){
+                return node.left;
+            }
+            if(node.left == null){
+                return node.right;
+            }
+            // 3
+
+            // Find node that will replace the deleted node(we'll use smallest node of node to be deletes right subtree)
+            Integer smallestValue = findSmallestValue(node.right);
+            // Assign the smallest value to the node to delete, and then delete it from right subtree
+            node.item = smallestValue;
+            node.right = delete(node.right, smallestValue);
+
+        }
+        if(item < node.item){
+            return delete(node.left, item);
+        } else {
+            return delete(node.right, item);
+        }
+    }
+
+    private Integer findSmallestValue(TreeNode root){
+        return root.left == null ? root.item : findSmallestValue(root.left);
     }
 
     public Integer size(){
@@ -125,11 +172,15 @@ public class BinaryTree {
         if(root.right == null) {
             return root.item;
         } else {
-            return minValue(node.right);
+            return maxValue(node.right);
         }
     }
 
-    /*If the tree is non-empty, then it consists of a root and two subtrees. Print the item in the root and use
+
+
+
+    /*
+    If the tree is non-empty, then it consists of a root and two subtrees. Print the item in the root and use
       recursion to print the items in the subtrees. This prints all the items on one line of output
       -- In a preorder traversal, the root node of the tree is processed first, then the left subtree is traversed,
             then the right subtree
